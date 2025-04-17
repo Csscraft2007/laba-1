e3dae5050c2110c042cf9d3f79fd1a34042dff94
Csscraft2007 <dp352590@gmail.com>
2025-04-17 12:35:01 +0700

    Update huh.py

--- huh.py
+++ huh.py
@@ -1,1 +1,12 @@
-print("123")
+def count_jewels_in_stones(J, S):
+   """
+    jewels = set(J)  # Создаем множество для быстрой проверки
+    count = 0
+    for stone in S:
+        if stone in jewels:
+            count += 1
+    return count
+J = "ab"
+S = "aabbccd"
+result = count_jewels_in_stones(J, S)
+print(result)  # Вывод: 4
